e91794076ce43f86a2bed7fa68ea626556d21072
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-01-03 14:40:49 +0100

    ft_tolower

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,8 +1,6 @@
 #include "libft.h"
 //#include <ctype.h>
 
-#include <ctype.h>
-
 int ft_toupper(int c)
 {
     if (c >= 97 && c <= 122)
--- libft.h
+++ libft.h
@@ -10,5 +10,7 @@ void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy(char *dest, const char *src, size_t n);
 size_t ft_strlcat(char *dest, const char *src, size_t size);
 size_t ft_strlen(const char *str);
+int ft_toupper(int c);
+int ft_tolower(int c);
 
 #endif
