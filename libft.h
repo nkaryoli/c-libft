30ae8a912414553e25afde15aa5577554bca89f8
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-01-03 17:19:46 +0100

    ft_strncmp

--- libft.h
+++ libft.h
@@ -14,5 +14,7 @@ int ft_toupper(int c);
 int ft_tolower(int c);
 char *ft_strchr(const char *str, int c);
 char *ft_strrchr(const char *str, int c);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+int ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
