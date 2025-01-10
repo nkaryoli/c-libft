cdf463f714f4e8b6b5b8ca73f1da1456c59faca7
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-01-10 23:54:02 +0100

    test01

--- main.c
+++ main.c
@@ -12,6 +12,37 @@
 
 #include "libft.h"
 
+//test01: ft_isalpha.c 
+//	ft_isdigit.c
+//	ft_isalnum.c
+//	ft_putnbr_fd.c
+//	ft_putendl.c
+
+void	test_01(int c)
+{
+	char	*y;
+	char	*z;
+	char	*e;
+	char	*f;
+
+	y = " es una letra";
+	z = " es un numero";
+	e = " no es alfanumerico";
+	f = " no es imprimible";
+	ft_putnbr_fd(c, 1);
+	if (ft_isalpha(c) == 1)
+		ft_putendl_fd(y, 1);
+	if (ft_isdigit(c) == 1)
+		ft_putendl_fd(z, 1);
+	if (ft_isalnum(c) == 0)
+		ft_putendl_fd(e, 1);
+	if (ft_isascii(c) == 0)
+	{
+		ft_putnbr_fd(c, 1);
+		ft_putendl_fd(f, 1);
+	}
+}
+
 int	main(void)
 {
 	int		c1;
@@ -30,7 +61,11 @@ int	main(void)
 	ft_putchar_fd(nl, fd);
 	c2 = ft_tolower(c2);
 	c = c2;
-	write(1, &c, 1);
 	ft_putchar_fd(c,fd);
+	ft_putchar_fd(nl, fd);
+	test_01(48);
+	test_01(97);
+	test_01(34);
+	test_01(128);
 	return (0);
 }
